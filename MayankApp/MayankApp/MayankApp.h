146bd6032b368b5538c87bb46fda70bbbf9ab1bb
legen12345dairy <rawat.mayank1234@gmail.com>
2019-12-11 17:20:35 +0530

    first commit

---
+++ MayankApp/MayankApp/MayankApp.h
@@ -0,0 +1,19 @@
+//
+//  MayankApp.h
+//  MayankApp
+//
+//  Created by Mayank Rawat on 11/12/19.
+//  Copyright © 2019 Mayank Rawat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MayankApp.
+FOUNDATION_EXPORT double MayankAppVersionNumber;
+
+//! Project version string for MayankApp.
+FOUNDATION_EXPORT const unsigned char MayankAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MayankApp/PublicHeader.h>
+
+

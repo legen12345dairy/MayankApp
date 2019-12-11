48b7dc1008c28865312207c982aaa4d1d4ef8462
Mayank Rawat <mayank.rawat@197NODMB25171.local>
2019-12-11 17:44:59 +0530

    class addedd

---
+++ MayankApp/BaseClass.swift
@@ -0,0 +1,15 @@
+//
+//  BaseClass.swift
+//  MayankApp
+//
+//  Created by Mayank Rawat on 11/12/19.
+//  Copyright © 2019 Mayank Rawat. All rights reserved.
+//
+
+import Foundation
+class BaseClass: NSObject {
+    override init() {
+        
+    }
+    
+}
